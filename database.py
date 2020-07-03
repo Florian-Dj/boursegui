a952c49ce5c8ddc3834a3615b0f386b38932f554
mucral <floriandjerbi@free.fr>
2020-07-03 15:01:46 +0200

    create first table on data.db

--- database.py
+++ database.py
@@ -4,14 +4,37 @@ import os
 import sqlite3
 from sqlite3 import Error
 
+data_name = "data.db"
 
-def create_data(file):
-    if os.path.exists(file):
-        print("Ok")
-    else:
-        print("Create file - test.sqlite")
-        open(file, "w")
+
+def create_data():
+    if not os.path.exists(data_name):
+        open(data_name, "w")
+
+
+def connection():
+    conn = None
+    try:
+        conn = sqlite3.connect(data_name)
+        return conn
+    except Error as e:
+        print(e)
+    return conn
+
+
+def create_table(co):
+    c = co.cursor()
+    c.execute("""
+        CREATE TABLE IF NOT EXISTS company (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            code TEXT NOT NULL
+        )""")
+    print("Table Company Create")
 
 
 if __name__ == '__main__':
-    create_data("data.db")
+    create_data()
+    conn = connection()
+    if conn is not None:
+        create_table(conn)

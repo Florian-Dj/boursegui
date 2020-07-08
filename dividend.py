3ad8d01dc69bde00737ee28b56447d2a28de6596
Mucral <43535702+Mucral@users.noreply.github.com>
2020-07-08 11:16:58 +0200

    Ajout des intêrets dividendes 2021 et 2022

--- dividend.py
+++ dividend.py
@@ -17,28 +17,33 @@ def home():
     if choose == "0":
         main.main()
     elif choose == "1":
-        list_dividend_2020()
+        list_dividend_now()
+    elif choose == "2":
+        list_dividend_20(1)
+    elif choose == "3":
+        list_dividend_20(2)
     else:
         print("\nMerci de choisir un choix valide")
         time.sleep(2)
         home()
 
 
-def list_dividend_2020():
+def list_dividend_now():
     results = database.select()
     if len(results) > 0:
+        print("\t\t----- Dividendes 2020 -----\n")
         for result in results:
             url = "https://www.boursorama.com/cours/" + result[2]
             req = requests.get(url)
             soup = BeautifulSoup(req.content, 'html.parser')
-            dividend_price = soup.find('li', class_="c-list-info__item c-list-info__item--fixed-width").text.replace(" ", "").split("\n")[3]
+            value_action = soup.find_all('span', class_="c-instrument c-instrument--last")[0].text
+            value_div = soup.find('li', class_="c-list-info__item c-list-info__item--fixed-width").text.replace(" ", "").split("\n")[3]
             dividend_date = soup.find_all('li', class_="c-list-info__item c-list-info__item--fixed-width")[1].text.replace(" ", "").split("\n")[3]
-            value = soup.find_all('span', class_="c-instrument c-instrument--last")[0].text
-            if dividend_price == "-":
+            if value_div == "-":
                 dividend_date = "?"
-                dividend_price = soup.find('td', class_="c-table__cell c-table__cell--dotted c-table__cell--inherit-height c-table__cell--align-top / u-text-left u-text-right u-ellipsis").text.replace(" ", "").replace("\n", "")
-            interest = round(float(dividend_price[:-3]) * 100 / float(value), 2)
-            print("{n} -  Prix: {dp}; Date: {dd}; Intêret: {i}%".format(n=result[1], dp=dividend_price, dd=dividend_date, i=interest))
+                value_div = soup.find('td', class_="c-table__cell c-table__cell--dotted c-table__cell--inherit-height c-table__cell--align-top / u-text-left u-text-right u-ellipsis").text.replace(" ", "").replace("\n", "")
+            interest = round(float(value_div[:-3]) * 100 / float(value_action), 2)
+            print("{n} -  Valeur: {v}; Date: {dd}; Intêret: {i}%".format(n=result[1], v=value_div, dd=dividend_date, i=interest))
         time.sleep(2)
         home()
     else:
@@ -46,3 +51,23 @@ def list_dividend_2020():
         time.sleep(2)
         home()
 
+
+def list_dividend_20(year):
+    results = database.select()
+    if len(results) > 0:
+        print("\t\t----- Dividendes 202{y} -----\n".format(y=year))
+        for result in results:
+            url = "https://www.boursorama.com/cours/" + result[2]
+            req = requests.get(url)
+            soup = BeautifulSoup(req.content, 'html.parser')
+            value_action = soup.find_all('span', class_="c-instrument c-instrument--last")[0].text
+            value_div = soup.find_all('td', class_="c-table__cell c-table__cell--dotted c-table__cell--inherit-height c-table__cell--align-top / u-text-left u-text-right u-ellipsis")[year].text.replace(" ", "").replace("\n", "")
+            dividend_date = soup.find_all('li', class_="c-list-info__item c-list-info__item--fixed-width")[1].text.replace(" ", "").split("\n")[3]
+            interest = round(float(value_div[:-3]) * 100 / float(value_action), 2)
+            print("{n} -  Valeur: {v}; Date: {dd}; Intêret: {i}%".format(n=result[1], v=value_div, dd=dividend_date, i=interest))
+        time.sleep(2)
+        home()
+    else:
+        print("\nAucune Entreprise dans la liste")
+        time.sleep(2)
+        home()

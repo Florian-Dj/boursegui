ef38a96cd252a5eaaabdcba5cc5b0f677f9adf2b
Mucral <43535702+Mucral@users.noreply.github.com>
2020-07-13 17:48:33 +0200

    Bug Fixe, erreur no company on run and run never stop

--- parse.py
+++ parse.py
@@ -37,56 +37,60 @@ def number_run():
 def parse(run):
     sql = """SELECT * FROM my_list"""
     results = database.select(sql)
-    day = datetime.datetime.today().weekday()
-    time_now = datetime.datetime.now()
-    morning = time_now.replace(hour=9, minute=00)
-    evening = time_now.replace(hour=17, minute=40)
-    i = 1
-    while i <= run:
-        if morning < time_now < evening and day != 5 and day != 6:
-            datetime_now = datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S")
-            print()
-            print("--- {t} ---".format(t=datetime_now))
-            print()
-            for result in results:
-                url = "https://www.boursorama.com/cours/" + result[2]
-                req = requests.get(url)
-                soup = BeautifulSoup(req.content, 'html.parser')
-                name = soup.find(class_="c-faceplate__company-link").text.replace(" ", "").replace("\n", "")
-                value = soup.find_all('span', class_="c-instrument c-instrument--last")[0].text
-                var = soup.find_all('span', class_="c-instrument c-instrument--variation")[0].text
-                volume = soup.find_all('span', class_="c-instrument c-instrument--totalvolume")[0].text.replace(" ", "")
-                vol_var = soup.find_all('li', class_="c-list-info__item--small-gutter")[2]
-                vol_var = vol_var.text.replace(" ", "").split("\n")[3]
-                dividend_date = soup.find_all('li', class_="c-list-info__item c-list-info__item--fixed-width")[1]
-                dividend_date = dividend_date.text.replace(" ", "").split("\n")[3].split(".")
-                dividend_date = "20{}-{}-{}".format(dividend_date[2], dividend_date[1], dividend_date[0])
-                value_div = []
-                value_div_20 = soup.find('li', class_="c-list-info__item c-list-info__item--fixed-width")
-                value_div_20 = value_div_20.text.replace(" ", "").split("\n")[3]
-                company_id = result[0]
-                if value_div_20 == "-":
-                    value_div_20 = soup.find('td', class_="c-table__cell c-table__cell--dotted c-table__cell--inherit-height"
-                                                       " c-table__cell--align-top / u-text-left u-text-right u-ellipsis")
-                    value_div.append(value_div_20.text.replace(" ", "").replace("\n", "").replace("EUR", ""))
-                else:
-                    value_div.append(value_div_20.replace("EUR", ""))
-                nb = 1
-                while nb <= 5:
-                    value_div_other = soup.find_all('td', class_="c-table__cell c-table__cell--dotted c-table__cell--inherit-height"
-                                                        " c-table__cell--align-top / u-text-left u-text-right u-ellipsis")[nb]
-                    value_div.append(value_div_other.text.replace(" ", "").replace("\n", "").replace("EUR", ""))
-                    nb += 1
-                company(company_id, value, var, volume, vol_var, datetime_now)
-                interest(company_id, value_div, dividend_date, datetime_now)
-                print("\t\t{n}\nAction: {val}€\t{var}\nVolume: {vo}\t{vov}\nDividende: {vd}€\t{vp}"
-                      .format(n=name, val=value, var=var, vo=volume, vov=vol_var, vd=value_div[0], vp=value_div[3]))
+    if results:
+        i = 1
+        while i <= run:
+            day = datetime.datetime.today().weekday()
+            time_now = datetime.datetime.now()
+            morning = time_now.replace(hour=9, minute=00)
+            evening = time_now.replace(hour=17, minute=50)
+            if morning < time_now < evening and day != 5 and day != 6:
+                datetime_now = datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S")
                 print()
-            time.sleep(6)
-        else:
-            print("Bourse fermée")
-            break
-        i += 1
+                print("--- {t} ---".format(t=datetime_now))
+                print()
+                for result in results:
+                    url = "https://www.boursorama.com/cours/" + result[2]
+                    req = requests.get(url)
+                    soup = BeautifulSoup(req.content, 'html.parser')
+                    name = soup.find(class_="c-faceplate__company-link").text.replace(" ", "").replace("\n", "")
+                    value = soup.find_all('span', class_="c-instrument c-instrument--last")[0].text
+                    var = soup.find_all('span', class_="c-instrument c-instrument--variation")[0].text
+                    volume = soup.find_all('span', class_="c-instrument c-instrument--totalvolume")[0].text.replace(" ", "")
+                    vol_var = soup.find_all('li', class_="c-list-info__item--small-gutter")[2]
+                    vol_var = vol_var.text.replace(" ", "").split("\n")[3]
+                    dividend_date = soup.find_all('li', class_="c-list-info__item c-list-info__item--fixed-width")[1]
+                    dividend_date = dividend_date.text.replace(" ", "").split("\n")[3].split(".")
+                    dividend_date = "20{}-{}-{}".format(dividend_date[2], dividend_date[1], dividend_date[0])
+                    value_div = []
+                    value_div_20 = soup.find('li', class_="c-list-info__item c-list-info__item--fixed-width")
+                    value_div_20 = value_div_20.text.replace(" ", "").split("\n")[3]
+                    company_id = result[0]
+                    if value_div_20 == "-":
+                        value_div_20 = soup.find('td', class_="c-table__cell c-table__cell--dotted c-table__cell--inherit-height"
+                                                           " c-table__cell--align-top / u-text-left u-text-right u-ellipsis")
+                        value_div.append(value_div_20.text.replace(" ", "").replace("\n", "").replace("EUR", ""))
+                    else:
+                        value_div.append(value_div_20.replace("EUR", ""))
+                    nb = 1
+                    while nb <= 5:
+                        value_div_other = soup.find_all('td', class_="c-table__cell c-table__cell--dotted c-table__cell--inherit-height"
+                                                            " c-table__cell--align-top / u-text-left u-text-right u-ellipsis")[nb]
+                        value_div.append(value_div_other.text.replace(" ", "").replace("\n", "").replace("EUR", ""))
+                        nb += 1
+                    company(company_id, value, var, volume, vol_var, datetime_now)
+                    interest(company_id, value_div, dividend_date, datetime_now)
+                    print("\t\t{n}\nAction: {val}€\t{var}\nVolume: {vo}\t{vov}\nDividende: {vd}€\t{vp}"
+                          .format(n=name, val=value, var=var, vo=volume, vov=vol_var, vd=value_div[0], vp=value_div[3]))
+                    print()
+                time.sleep(60)
+            else:
+                print("Bourse fermée")
+                break
+            i += 1
+    else:
+        print("\nAucune Entreprise dans la liste")
+        time.sleep(2)
     main.main()
 
 
